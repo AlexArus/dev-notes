893af8f85a098e0f3a27864e780c0345f093c146
AlexArus <mr.alex.arus@gmail.com>
2020-04-13 22:19:56 +0300

    fix service worker

--- flashcards/service-worker.js
+++ flashcards/service-worker.js
@@ -1,33 +1,53 @@
-const CACHE_NAME = 'static-cache-v1';
+/*
+https://codelabs.developers.google.com/codelabs/your-first-pwapp/#0
+https://developers.google.com/web/fundamentals/primers/service-workers
+*/
 
-const FILES_TO_CACHE = [
-    'index.html',
+var CACHE_NAME = 'site-cache-v1';
+var urlsToCache = [
+    'index.html'
 ];
 
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-    })
-);
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
 
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                return caches.delete(key);
-            }
-        }));
-    })
-);
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
 
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                    return cache.match('index.html');
-                });
-        })
-);
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
